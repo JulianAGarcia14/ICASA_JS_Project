eacc5a50bc72a0732d8e47f0fc76a1574ecf957f
Julian Garcia <jgarcia@icasa.nmt.edu>
2021-05-17 18:08:39 -0600

    Large first commit. Included code visualizes a erdos-reyni network using the network graphing feature of vis.js. Serves an html which displays the graph using Node.js and express.js. Currently, the probability and number of nodes to construct the network are all hard coded. serveNetwork.js is all that needs to be run. The html, css, and javascript responsible for drawing the network are in the 'public' directory. Developed this in intellij, pushed the entire contents of my working directory, so some parts should most likely be removed later on.

---
+++ public/drawNet.js
@@ -0,0 +1,34 @@
+function erdos(prob, vertNum) {
+
+    var nodes = [];
+    var edges = [];
+
+
+    for (var i = 0; i < vertNum; i++) {
+        nodes.push({id: i, label: 'Node'+ i});
+    }
+
+    for (var i = 0; i < vertNum; i++) {
+        for (var j = i+1; j < vertNum; j++) {
+            var p = Math.random();
+            if(p > prob) {
+                edges.push({from: i, to: j});
+            }
+        }
+    }
+
+    var data= {
+        nodes: nodes,
+        edges: edges,
+    };
+    return data;
+}
+
+// create a network
+var container = document.getElementById('mynetwork');
+let data = erdos(0.8, 10);
+
+var options = {};
+
+// initialize your network!
+var network = new vis.Network(container, data, options);
